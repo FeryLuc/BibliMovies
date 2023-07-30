0d687353086c58c90cd1326eeb610c449bb362e4
unknown <Lucasus822@gmail.com>
2023-07-30 14:39:16 +0200

    Homepage Frontend

---
+++ js/script.js
@@ -0,0 +1,3 @@
+let movies = ["Comédie", "Thriller", "Action", "Horreur", "Science-fiction", "Fantastique", "Animation", "Documentaire", "Drame", "Biopic", "Romantique", "Historique", "Court-métrage", "Aventure"];
+
+console.log(movies.sort());
