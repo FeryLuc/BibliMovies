288023088c2712ebc6e1c964d415e72eb675961a
Luc Fery <Lucasus822@gmail.com>
2024-01-27 08:50:23 +0100

    start details

--- scripts/detailsScript.js
+++ scripts/detailsScript.js
@@ -2,6 +2,12 @@ import { config, options, addLanguageToUrl, formattingDate, pages } from '../con
 const urls = config;
 const optns = options
 const urlWithLang = addLanguageToUrl;
+const poster = document.getElementById('poster');
+const backdrop = document.getElementById('backdrop');
+const title = document.getElementById('title');
+const tagline = document.getElementById('tagline');
+const description = document.getElementById('desc');
+const genresList = document.getElementById('genres');
 
 if (localStorage.getItem('movieId') !== null) {
     const maDonnee = JSON.parse(localStorage.getItem('movieId'));
@@ -21,11 +27,38 @@ async function fetchMovie(url, id) {
             throw new Error("Network response not ok !");
         }
         let data = await response.json();
+        let genres = data.genres;
         console.log(data);
+        doWithResult(data, genres);
         localStorage.clear();
     } catch (error) {
         console.error(error);
     }
+}
+
+function doWithResult(data, genres) {
+    let movieTitle = data.title;
+    if ('name' in data) {
+        movieTitle = data.name;
+    }
+    backdrop.style.backgroundImage = `url(${urls.imgBaseUrl+data.backdrop_path})`;
+    poster.src = urls.imgBaseUrl+data.poster_path;
+    
+    if (data.tagline !== "") {
+        title.textContent = movieTitle+' - ';
+        tagline.textContent = data.tagline;
+    }else{
+        title.textContent = movieTitle;
+    }
+    
+    for (const genre of genres) {
+        const genreItem = document.createElement('li');
+        genreItem.textContent = genre.name;
+        genresList.appendChild(genreItem);
+    }
+    
+
+    description.textContent = data.overview;
 }
 
 for (let i = 0; i < localStorage.length; i++) {
